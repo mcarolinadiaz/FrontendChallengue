11002057163b727691c4ccbaabe401d937f2008d
mcarolinadiaz <diazmariacarolina6@gmail.com>
2023-04-26 14:41:43 -0300

    Fresh News: grid was added

--- src/app/main/fresh-news/fresh-news.component.ts
+++ src/app/main/fresh-news/fresh-news.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FreshNewsComponent implements OnInit {
 
+  freshNews: any = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.freshNews = this.getFreshNews();
   }
+
+  getFreshNews(): any[] {
+    this.freshNews = [
+      {
+        "title": "Chin chin por nuestra plata en el #hackforgood del cdec",
+        "image": "src/assets/images/news-01.jpg",
+        "more": ""
+      },
+      {
+        "title": "Chin chin por nuestra plata en el #hackforgood del cdec",
+        "image": "src/assets/images/news-02.jpg",
+        "more": ""
+      },
+      {
+        "title": "Dos alumnas de Brother crean la primera manifestación feminista en Twitch",
+        "image": "src/assets/images/news-02.jpg",
+        "more": ""
+      },
+      {
+        "title": "#PROHIBIDOFIJARCARTELES: UNA NUEVA OPORTUNIDAD PARA EL CARTEL MÁS FEO DEL MUNDO",
+        "image": "",
+        "more": ""
+      },
+      {
+        "title": "Brother Presenta Pinta y Colorea: el ciclo con el mejor talento gráfico de Madrid",
+        "image": "src/assets/images/news-03.jpg",
+        "more": ""
+      },
+      {
+        "title": "Somos la escuela con más finalistas en el Festival CDEC (otra vez ;) )",
+        "image": "",
+        "more": ""
+      },
+      {
+        "title": "Las nuevas ideas llegan a correos gracias a los alumnos de Brother",
+        "image": "",
+        "more": ""
+      },
+      {
+        "title": "Nueva representación del kingdom a cargo del ilustrador asis percales",
+        "image": "",
+        "more": ""
+      }
+    ];
+    return this.freshNews;
+  }
+
+
+
+
 
 }
